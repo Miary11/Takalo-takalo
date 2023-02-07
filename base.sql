50250b26fbc527523016278bfdf92c047648fe7c
wahuan23 <120554881+wahuan23@users.noreply.github.com>
2023-02-07 09:04:59 +0100

    c3

--- base.sql
+++ base.sql
@@ -30,5 +30,12 @@ insert into Utilisateur values(null ,'Rabenarivo Miantsa','Miantsa12@gmail.com',
 insert into Utilisateur values(null ,'Rakotoson Naina','Nainaraks@gmail.com','artiste23');
 insert into Utilisateur values(null ,'Rajoelina Tsito','Tsitorajoelina@gmail.com','mits34');
 
+insert into Objet values(null,1,'montre rolex','montre1.jpg',200,'montre personnalisée noire,avec une minuterie',4);
+insert into Objet values(null,2,'montre watch','montre2.jpg',150,'montre noire,version2 avec blueotooth',4);
+insert into Objet values(null,3,'cargo homme','pantalon1.jpg',50,'cargo beige pour homme taille S',1);
+insert into Objet values(null,4,'pantalon noir','pantalon2.jpg',30,'pantalon noir taille L',1);
+insert into Objet values(null,5,'dvd Coraline','dvd1.jpg',10,'dessin animé Coraline',2);
+
+
 
 
